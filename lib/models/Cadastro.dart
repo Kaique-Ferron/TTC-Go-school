060e76ca16ef1653c04f8edcf43e0b530c2dc872
Aluno <e023aluno@outlook.com>
2026-09-02 19:53:09 -0300

    Cadastro-models-pronto

---
+++ lib/models/Cadastro.dart
@@ -0,0 +1,22 @@
+class Cadastro {
+  String nome;
+  String email;
+  int telefone;
+  int cpf;
+  int nascimento;
+  String senha;
+
+
+
+
+
+  Cadastro({
+    required this.nome,
+    required this.email,
+    required this.telefone,
+    required this.cpf,
+    required this.nascimento,
+    required this.senha,
+  });
+
+}
