b823cdef41dcdbd8925f719e033f87b86087f038
Dev-team-KATO <dev.app.hq@gmail.com>
2026-09-10 11:38:15 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
-
-// Importe o arquivo da tela que acabamos de criar
-import 'telas/landpage/landpage-tela.dart';
+import 'app/app.dart';
 
 void main() {
-  runApp(const MeuApp());
-}
-
-class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Go School',
-      // Remove a faixa vermelha de "DEBUG" do canto da tela
-      debugShowCheckedModeBanner: false, 
-      theme: ThemeData(
-        primaryColor: const Color(0xFF1D58E2),
-        scaffoldBackgroundColor: const Color(0xFFF4F5F7),
-      ),
-      // Aqui você define qual tela o app vai abrir primeiro:
-      home: const LandpageTela(), 
-    );
-  }
+  runApp(const GoSchoolApp());
 }
