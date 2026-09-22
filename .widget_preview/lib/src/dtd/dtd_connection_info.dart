669292f35bd2c04e2d8ea7bbe98e10c020a4eb7d
Marcos Ryan <marcosryanjesus@gmail.com>
2026-09-22 14:29:43 -0300

    Limpeza de dependencias e arquivos temporarios

---
+++ .widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:56975/KmnuvzgIKVk=';
+const String kWidgetPreviewService =
+    'widget-preview-fcaea754-6a2c-472a-b37d-2e0eb2daa74d';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-fcaea754-6a2c-472a-b37d-2e0eb2daa74d';
+const String kProjectRootPath =
+    r'C:\Users\Ryan de Jesus\OneDrive\Desktop\TTC-Go-school';
